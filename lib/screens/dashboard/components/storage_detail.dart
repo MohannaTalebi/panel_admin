c4e43e94f121ded6cdfb26ba5e96e5021b886af4
Mohanna <mohannatalebi64@gmail.com>
2023-04-22 01:06:33 +0330

    Update storage

---
+++ lib/screens/dashboard/components/storage_detail.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:panel_admin/constants.dart';
+import 'package:panel_admin/screens/dashboard/components/dashoard_widget.dart';
+import 'package:panel_admin/screens/dashboard/components/mystorage_card.dart';
+
+
+
+
+class storege_detail extends StatelessWidget {
+  const storege_detail({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: secondaryColor,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Storage Detail',
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 18,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          mychart(),
+          mystorage_card(
+            icon: Icons.file_copy,
+            title: 'Document',
+            numberspace: ' 1.3GB',
+            color: primaryColor,
+          ),
+          mystorage_card(
+            icon: Icons.perm_media,
+            title: 'Media files',
+            numberspace: '2.5GB',
+            color: Color(0xFF26E5FF),
+          ),
+          mystorage_card(
+            icon: Icons.folder,
+            title: 'Others files',
+            numberspace: '3.20GB',
+            color: Color(0xFFFFCF26),
+          ),
+          mystorage_card(
+            icon: Icons.folder_off,
+            title: 'Unknown',
+            numberspace: '3.70GB',
+            color: Color(0xFFEE2727),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -3,6 +3,7 @@ import 'package:panel_admin/constants.dart';
 import 'package:panel_admin/screens/dashboard/components/dashoard_widget.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:panel_admin/screens/dashboard/components/mystorage_card.dart';
+import 'package:panel_admin/screens/dashboard/components/storage_detail.dart';
 
 class dashboard_screen extends StatefulWidget {
   const dashboard_screen({Key? key}) : super(key: key);
@@ -24,69 +25,57 @@ class _dashboard_screenState extends State<dashboard_screen> {
               height: 16,
             ),
             Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Expanded(
-                  flex: 5,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.white,
-                    ),
-                    height: 500,
-                  ),
-                ),
+                    flex: 5,
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'My Files',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            ElevatedButton.icon(
+                              style: TextButton.styleFrom(
+                                padding: EdgeInsets.all(16),
+                              ),
+                              onPressed: () {},
+                              icon: Icon(Icons.add),
+                              label: Text('Add New'),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        GridView.builder(
+                          itemCount: 4,
+                          shrinkWrap: true,
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 4,
+                                crossAxisSpacing: 16,
+                          ),
+                          itemBuilder: (context, index) => Container(
+                            decoration: BoxDecoration(
+                              color: secondaryColor,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )),
                 SizedBox(
                   width: 16,
                 ),
                 Expanded(
                   flex: 2,
-                  child: Container(
-                    padding: EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: secondaryColor,
-                    ),
-                    child: Column(
-                      children: [
-                        Text(
-                          'Storage Detail',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: 18,
-                            color: Colors.white,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 16,
-                        ),
-                        mychart(),
-                        mystorage_card(
-                          icon: Icons.file_copy,
-                          title: 'Document',
-                          numberspace:' 1.3GB',
-                          color:primaryColor,
-                        ),
-                        mystorage_card(
-                          icon:Icons.perm_media ,
-                          title:'Media files' ,
-                          numberspace:'2.5GB' ,
-                         color: Color(0xFF26E5FF),
-                        ),
-                        mystorage_card(
-                          icon:Icons.folder ,
-                          title:'Others files' ,
-                          numberspace: '3.20GB',
-                          color: Color(0xFFFFCF26),
-                        ),
-                        mystorage_card(
-                          icon:Icons.folder_off ,
-                          title:'Unknown' ,
-                          numberspace: '3.70GB',
-                          color:Color(0xFFEE2727),
-                        ),
-                      ],
-                    ),
-                  ),
+                  child: storege_detail(),
                 ),
               ],
             ),
